707c8d4151931c2b077c24cf4da8edcd9f802876
Jullius <elbaz0708@gmail.com>
2023-01-05 22:25:16 +0100

    'againVideo'

--- components/Global/Card/VideoCard.js
+++ components/Global/Card/VideoCard.js
@@ -1,6 +1,6 @@
 import { Modal, StyleSheet, Text, View } from "react-native";
 import { RFPercentage } from "react-native-responsive-fontsize";
-import { ResizeMode, Video } from "expo-av";
+import { ResizeMode, AVPlaybackStatus, Video } from "expo-av";
 import { _URL } from "../../../globalVar/url";
 import { useEffect, useRef, useState } from "react";
 import { color } from "react-native-reanimated";
@@ -8,7 +8,7 @@ import GlobalStyles from "../../../style/GlobalStyles";
 import ModalGlobal from "../Modal";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import VideoPlayer from "expo-video-player";
-import { AntDesign } from '@expo/vector-icons';
+import { AntDesign } from "@expo/vector-icons";
 
 export default function VideoCard(props) {
   const video = useRef(null);
@@ -16,13 +16,18 @@ export default function VideoCard(props) {
   const [status, setStatus] = useState({});
   const [videoTime, setVideoTime] = useState("");
   const [loading, setLoading] = useState(true);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [isPaused, setIsPaused] = useState(false);
+  const [isBuffering, setIsBuffering] = useState(false);
+  const [videoMilli, setVideoMilli] = useState(0)
+  const [progressIndicator, setProgressIndicator] = useState(0);
 
   useEffect(() => {
-    console.log("status", video.current);
+    console.log(((status.positionMillis / status.durationMillis) * 100).toFixed(2))
     if (status.isLoaded === true) {
       convertMillisecondsToTime(status.durationMillis);
     }
-  }, [status.isLoaded]);
+  }, [progressIndicator]);
 
   const convertMillisecondsToTime = (milliseconds) => {
     var minutes = Math.floor(milliseconds / 60000);
@@ -34,10 +39,24 @@ export default function VideoCard(props) {
     setLoading((prev) => (prev = false));
   };
 
-  const handleFullscreenUpdate = (event) => {};
+  const quitVideo = () => {
+    setProgressIndicator(
+      (prev) => (prev =  ((status.positionMillis / status.durationMillis) * 100).toFixed(2))
+    );
+    setVideoMilli( prev => prev = status.positionMillis)
+   
+    setIsOpen(false);
+  };
+
+ const  openVideo = () => {
+   setIsOpen(true)
+   if (isOpen === true) {
+     console.log(status.positionMillis)
+   }
+  }
 
   return (
-    <TouchableOpacity onPress={() => setIsOpen(true)} style={styles.card}>
+    <TouchableOpacity onPress={() => openVideo( )} style={styles.card}>
       <View style={styles.image}></View>
       <View style={styles.infoCont}>
         <View>
@@ -47,7 +66,16 @@ export default function VideoCard(props) {
             {videoTime} min
           </Text>
         </View>
-        <View style={styles.progress}></View>
+        <View style={styles.progress}>
+          <View
+            style={{
+              backgroundColor: GlobalStyles.primary.color,
+              height: "100%",
+              width: `${progressIndicator}%`,
+              borderRadius: 15,
+            }}
+          ></View>
+        </View>
       </View>
       <Modal
         animationType="fade"
@@ -55,22 +83,36 @@ export default function VideoCard(props) {
         visible={isOpen}
         statusBarTranslucent
       >
-        <AntDesign onPress={()=> setIsOpen(false)} style={{position:'absolute', top:30 , left:20 , zIndex:10}} name="close" size={28} color="white" />    
-        <VideoPlayer 
-          videoProps={{
-            resizeMode: ResizeMode.COVER,
-            ref: video,
-            shouldPlay: true,
-            source: {
-              uri: `${_URL}${props.item.media.data.attributes.url}`,
-            },
-          }}
-          fullscreen={true} 
-            slider={{
-        visible: true,
-          }}
+        <AntDesign
+          onPress={() => quitVideo()}
           style={{
+            position: "absolute",
+            top: 30,
+            left: 20,
+            zIndex: 10,
+            width: 50,
+            height: 50,
           }}
+          name="close"
+          size={28}
+          color="white"
+        />
+
+        <Video
+          ref={video}
+          source={{ uri: `${_URL}${props.item.media.data.attributes.url}` }}
+          resizeMode="cover"
+          fullscreen={false}
+          shouldPlay
+          positionMillis={videoMilli}
+          useNativeControls
+          onError={(error) => console.log(error)}
+          onLoad={() => ''}
+          onLoadStart={() => ''}
+          onPlaybackStatusUpdate={(status) => {
+            setStatus((prev) => (prev = status));
+          }}
+          style={styles.video}
         />
       </Modal>
     </TouchableOpacity>
@@ -123,6 +165,6 @@ const styles = StyleSheet.create({
   },
 
   video: {
-    height: "0%",
+    minHeight: "100%",
   },
 });
